f9074011e564e6b70eec61782fb9adb1dc1b042b
coze <ilakozejumanne@gmail.com>
2016-04-03 16:54:53 +0300

    fixed monthly plan filtering issue

--- app/src/main/java/mobile/giis/app/fragments/MonthlyPlanFragment.java
+++ app/src/main/java/mobile/giis/app/fragments/MonthlyPlanFragment.java
@@ -1,9 +1,11 @@
 package mobile.giis.app.fragments;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
@@ -20,9 +23,13 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
@@ -74,8 +81,16 @@ public class MonthlyPlanFragment extends android.support.v4.app.Fragment{
 
     public ProgressBar loadingBar;
 
+
+    final DatePickerDialog fromDatePicker = new DatePickerDialog();
+    final DatePickerDialog toDatePicker = new DatePickerDialog();
+    private  MaterialEditText metDOBFrom,metDOBTo;
+    private String toDateString="",fromDateString="";
+
+    private EditText editTextUsedToRequestFocus;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_monthly_plan, null);
+        final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_monthly_plan, null);
         setUpView(root);
 
         app = (BackboneApplication) this.getActivity().getApplication();
@@ -89,6 +104,10 @@ public class MonthlyPlanFragment extends android.support.v4.app.Fragment{
         next            = (ImageButton) listviewFooter.findViewById(R.id.next_10_contents);
         prevLayout              = (RelativeLayout) listviewFooter.findViewById(R.id.prev_rl);
         nextLayout              = (RelativeLayout) listviewFooter.findViewById(R.id.next_rl);
+
+
+        editTextUsedToRequestFocus = (EditText) root.findViewById(R.id.edit_text_used_to_request_focus2);
+        editTextUsedToRequestFocus.requestFocus();
 
 
         nextLayout.setOnClickListener(new View.OnClickListener() {
@@ -110,6 +129,88 @@ public class MonthlyPlanFragment extends android.support.v4.app.Fragment{
                 new filterList().execute(currentCategory, currentCount);
             }
         });
+
+
+
+        metDOBFrom      = (MaterialEditText) root.findViewById(R.id.met_dob_from);
+        metDOBTo        = (MaterialEditText) root.findViewById(R.id.met_dob_value);
+
+        metDOBTo.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    toDatePicker.show(((Activity) getActivity()).getFragmentManager(), "DatePickerDialogue");
+                    toDatePicker.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                            metDOBTo.setText((dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth) + "-" + ((monthOfYear + 1) < 10 ? "0" + (monthOfYear + 1) : monthOfYear + 1) + "-"
+                                    + year);
+
+                            Calendar toCalendar = Calendar.getInstance();
+                            toCalendar.set(year, monthOfYear, dayOfMonth);
+                            fromDatePicker.setMaxDate(toCalendar);
+                            toDateString = (toCalendar.getTimeInMillis() / 1000) + "";
+                            editTextUsedToRequestFocus.requestFocus();
+
+                            if (!fromDateString.equals("")) {
+                                new filterList().execute(currentCategory, "0", fromDateString, toDateString);
+                            } else {
+                                final Snackbar snackbar = Snackbar.make(root, "Please select a start date to view the chart", Snackbar.LENGTH_LONG);
+                                snackbar.setAction("OK", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        snackbar.dismiss();
+                                    }
+                                });
+                                snackbar.show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+
+        metDOBFrom.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    fromDatePicker.show(((Activity) getActivity()).getFragmentManager(), "DatePickerDialogue");
+                    fromDatePicker.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                            metDOBFrom.setText((dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth) + "-" + ((monthOfYear + 1) < 10 ? "0" + (monthOfYear + 1) : monthOfYear + 1) + "-"
+                                    + year);
+
+                            Calendar fromCalendar = Calendar.getInstance();
+                            fromCalendar.set(year, monthOfYear, dayOfMonth);
+                            toDatePicker.setMinDate(fromCalendar);
+                            fromDateString = (fromCalendar.getTimeInMillis() / 1000) + "";
+                            editTextUsedToRequestFocus.requestFocus();
+
+                            if (!toDateString.equals("")) {
+                                new filterList().execute(currentCategory, "0", fromDateString, toDateString);
+                            } else {
+                                final Snackbar snackbar = Snackbar.make(root, "Please select an end date to view the chart", Snackbar.LENGTH_LONG);
+                                snackbar.setAction("OK", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        snackbar.dismiss();
+                                    }
+                                });
+                                snackbar.show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+
+
+
 
 
         ArrayList<String> ageDefStr = new ArrayList<>();
@@ -276,6 +377,19 @@ public class MonthlyPlanFragment extends android.support.v4.app.Fragment{
             String startRow = params[1];
             currentCount = params[1];
 
+            Calendar calendar = Calendar.getInstance();
+            String toDate = ((calendar.getTimeInMillis()+(30*24*60*60*1000))/1000)+"";
+
+            String from_date ="";
+            String to_date ="";
+
+            try{
+                from_date = params[2];
+                to_date = params[3];
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
             Cursor cursor;
             mVar = new ArrayList<>();
 
@@ -284,8 +398,10 @@ public class MonthlyPlanFragment extends android.support.v4.app.Fragment{
                             " ,(SELECT GROUP_CONCAT(dose.FULLNAME) FROM vaccination_event INNER JOIN dose ON vaccination_event.DOSE_ID = dose.ID left join age_definitions on dose.TO_AGE_DEFINITON_ID = age_definitions.ID WHERE monthly_plan.APPOINTMENT_ID=vaccination_event.APPOINTMENT_ID AND datetime(substr(vaccination_event.SCHEDULED_DATE,7,10), 'unixepoch') <= datetime('now','+30 days') AND vaccination_event.IS_ACTIVE='true' AND vaccination_event.VACCINATION_STATUS='false' AND (vaccination_event.NONVACCINATION_REASON_ID=0  OR vaccination_event.NONVACCINATION_REASON_ID in (Select ID from nonvaccination_reason where KEEP_CHILD_DUE = 'true')) AND (DAYS IS NULL or (datetime(substr(vaccination_event.SCHEDULED_DATE,7,10),'unixepoch') > datetime('now','-' || DAYS || ' days')) )) AS VACCINES " +
                             " , SCHEDULE, SCHEDULED_DATE "+
                             " FROM monthly_plan join dose on DOSE_ID = dose.ID" +
-                            " WHERE HEALTH_FACILITY_ID = '"+app.getLOGGED_IN_USER_HF_ID()+"' AND SCHEDULE like '%"+ageName+"%' AND datetime(substr(SCHEDULED_DATE,7,10), 'unixepoch') <= datetime('now','+30 days') "+
-                            "AND ( (Select DAYS from age_definitions WHERE ID = dose.TO_AGE_DEFINITON_ID ) IS NULL \n" +
+                            " WHERE HEALTH_FACILITY_ID = '"+app.getLOGGED_IN_USER_HF_ID()+"' AND SCHEDULE like '%"+ageName+"%' " +
+                            "AND datetime(substr(SCHEDULED_DATE,7,10), 'unixepoch') <= datetime('"+to_date+"','unixepoch') "+
+                            "AND datetime(substr(SCHEDULED_DATE,7,10), 'unixepoch') > datetime('"+from_date+"','unixepoch') "+
+                            "AND ( (Select DAYS from age_definitions WHERE ID = dose.TO_AGE_DEFINITON_ID ) IS NULL " +
                             " OR (datetime(substr(SCHEDULED_DATE,7,10),'unixepoch') > datetime('now','-' || (Select DAYS from age_definitions WHERE ID = dose.TO_AGE_DEFINITON_ID ) || ' days' )) )" +
                             " GROUP BY APPOINTMENT_ID, SCHEDULED_DATE, DOMICILE, NAME, SCHEDULE, CHILD_ID, SCHEDULE_ID "+
                             " ORDER BY SCHEDULED_DATE "+
