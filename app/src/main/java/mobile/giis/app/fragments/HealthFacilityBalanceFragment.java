c63c17dc8d7716749103df8e8ee360ea9e857bd0
issyzac <issyzac.iz@gmail.com>
2016-04-03 19:14:26 +0300

    health facility balance like before

--- app/src/main/java/mobile/giis/app/fragments/HealthFacilityBalanceFragment.java
+++ app/src/main/java/mobile/giis/app/fragments/HealthFacilityBalanceFragment.java
@@ -1,24 +1,43 @@
 package mobile.giis.app.fragments;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import mobile.giis.app.R;
+import mobile.giis.app.StockMenuActivity;
+import mobile.giis.app.adapters.AdapterHealthFacilityBalance;
 import mobile.giis.app.adapters.HealthFacilityBalanceListAdapter;
 import mobile.giis.app.base.BackboneActivity;
 import mobile.giis.app.base.BackboneApplication;
 import mobile.giis.app.database.DatabaseHandler;
 import mobile.giis.app.entity.HealthFacilityBalance;
+import mobile.giis.app.util.Constants;
 
 /**
  * Created by issymac on 09/02/16.
@@ -36,6 +55,8 @@ public class HealthFacilityBalanceFragment extends Fragment {
     private BackboneApplication application;
 
     private DatabaseHandler database;
+
+    private TableLayout hfBalanceTable;
 
     public static HealthFacilityBalanceFragment newInstance() {
         HealthFacilityBalanceFragment f = new HealthFacilityBalanceFragment();
@@ -59,10 +80,7 @@ public class HealthFacilityBalanceFragment extends Fragment {
         database = application.getDatabaseInstance();
         rowCollectorList = getHealthFacilityBalanceRows();
 
-        adapter = new HealthFacilityBalanceListAdapter(HealthFacilityBalanceFragment.this.getActivity(), rowCollectorList);
-
-        ListView stockAdjustmentList    = (ListView) root.findViewById(R.id.health_facility_balance_list);
-//        View v                          = inflater.inflate(R.layout.health_facility_balance_list_item_title, null);
+        hfBalanceTable  = (TableLayout) root.findViewById(R.id.hf_balance_table);
 
         TextView nameTitle       = (TextView) root.findViewById(R.id.txt_name_title);
         nameTitle.setTypeface(BackboneActivity.Rosario_Regular);
@@ -72,15 +90,130 @@ public class HealthFacilityBalanceFragment extends Fragment {
         expiryDateTitle.setTypeface(BackboneActivity.Rosario_Regular);
         TextView balanceTitle = (TextView) root.findViewById(R.id.txt_balance_title);
         balanceTitle.setTypeface(BackboneActivity.Rosario_Regular);
+        TextView editBalance = (TextView) root.findViewById(R.id.dozi_title);
+        editBalance.setTypeface(BackboneActivity.Rosario_Regular);
+        Button saveButton = (Button) root.findViewById(R.id.savebtn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveButtonClicked();
+            }
+        });
 
-//        stockAdjustmentList.addHeaderView(v);
-        stockAdjustmentList.setAdapter(adapter);
+
+
+        fillHFBalanceTable();
 
         return root;
     }
 
     public void setUpView(View v){
 
+    }
+
+    public void fillHFBalanceTable(){
+
+        for (HealthFacilityBalance items : rowCollectorList){
+            final HealthFacilityBalance item = items;
+            View rowView = View.inflate(HealthFacilityBalanceFragment.this.getActivity(), R.layout.health_facility_balance_list_item, null);
+            TextView name = (TextView) rowView.findViewById(R.id.item_name);
+            name.setTypeface(BackboneActivity.Rosario_Regular);
+            TextView lotNumber = (TextView) rowView.findViewById(R.id.lot_number);
+            lotNumber.setTypeface(BackboneActivity.Rosario_Regular);
+            TextView expiryDate = (TextView) rowView.findViewById(R.id.expiry_date);
+            expiryDate.setTypeface(BackboneActivity.Rosario_Regular);
+            TextView balance = (TextView) rowView.findViewById(R.id.balance);
+            balance.setTypeface(BackboneActivity.Rosario_Regular);
+            final MaterialEditText changed = (MaterialEditText) rowView.findViewById(R.id.hf_balance_adjustment_value);
+
+            name.setText(item.getItem_name().toString());
+            lotNumber.setText(item.getLot_number());
+            SimpleDateFormat ft = new SimpleDateFormat("dd-MMM-yyyy");
+            Log.d("Setting exp date", ft.format(BackboneActivity.dateParser(item.getExpire_date())));
+            if(calculateDateDiffToExpiryRowColor(BackboneActivity.dateParser(item.getExpire_date()))){
+                expiryDate.setBackgroundColor(Color.YELLOW);
+            }
+            expiryDate.setText(ft.format(BackboneActivity.dateParser(item.getExpire_date())));
+            balance.setText(item.getBalance() + "");
+
+            changed.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    item.setTempBalance(changed.getText().toString());
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+            hfBalanceTable.addView(rowView);
+        }
+    }
+
+    public void saveButtonClicked() {
+            boolean success = true;
+            for (HealthFacilityBalance healthFacilityBalance : rowCollectorList){
+                if(!healthFacilityBalance.getTempBalance().equals("")){
+                    healthFacilityBalance.setBalance(Integer.parseInt(healthFacilityBalance.getTempBalance()));
+                    if(database.updateHealthFacilityBalance(healthFacilityBalance) != -1) {
+                        startThread(healthFacilityBalance);
+                    }else{
+                        Toast.makeText(HealthFacilityBalanceFragment.this.getActivity(), "Not saved", Toast.LENGTH_LONG).show();
+                        success= false;
+                    }
+                }
+            }
+            if(success){
+
+                showDialogWhenSavedSucc();
+                hfBalanceTable.removeAllViews();
+                rowCollectorList = getHealthFacilityBalanceRows();
+                fillHFBalanceTable();
+            }
+    }
+
+    public void showDialogWhenSavedSucc() {
+        final Dialog d = new Dialog(HealthFacilityBalanceFragment.this.getActivity());
+        d.setContentView(R.layout.layout_stock_child);
+        TextView tvView = (TextView)d.findViewById(R.id.textView2);
+        tvView.setText(getString(R.string.saved_successfully));
+
+        d.show();
+
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        HealthFacilityBalanceFragment.this.getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(d!=null)d.dismiss();
+                            }
+                        });
+                    }
+                },
+                3500
+        );
+    }
+
+    public boolean calculateDateDiffToExpiryRowColor(Date expiry) {
+        Date now = new Date();
+        long diff = getDaysDifference(now, expiry);
+        return diff < Constants.LimitNumberOfDaysBeforeExpireVal;
+    }
+
+    public static final long  getDaysDifference(Date d1, Date d2){
+        long diff = d2.getTime() - d1.getTime();
+        long difference = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        return difference;
     }
 
     private ArrayList<HealthFacilityBalance> getHealthFacilityBalanceRows(){
@@ -113,6 +246,31 @@ public class HealthFacilityBalanceFragment extends Fragment {
         }
 
         return list;
+    }
+
+    private void startThread(HealthFacilityBalance healthFacilityBalance){
+        new Thread(){
+            HealthFacilityBalance healthFacilityBalance;
+            public Thread setData(HealthFacilityBalance healthFacilityBalance) {
+                this.healthFacilityBalance = healthFacilityBalance;
+                return this;
+            }
+
+            @Override
+            public void run() {
+                super.run();
+                BackboneApplication backbone = (BackboneApplication) HealthFacilityBalanceFragment.this.getActivity().getApplication();
+
+                try {
+                    backbone.saveHealthFacilityBalance(URLEncoder.encode(healthFacilityBalance.getGtin(), "utf-8")
+                            , URLEncoder.encode(healthFacilityBalance.getLot_number(), "utf-8")
+                            , healthFacilityBalance.getBalance()+""
+                            , URLEncoder.encode(new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime()), "utf-8")
+                            , URLEncoder.encode(backbone.getLOGGED_IN_USER_ID(), "utf-8"));
+                }catch (Exception e){e.printStackTrace();}
+
+            }
+        }.setData(healthFacilityBalance).start();
     }
 
 }
