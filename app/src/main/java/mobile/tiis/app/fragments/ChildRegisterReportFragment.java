52a5e953b22979592dff3522611349b10bcd49ea
coze <ilakozejumanne@gmail.com>
2016-07-25 15:31:03 +0300

    Implemented filtering of child register report by vaccination dates

--- app/src/main/java/mobile/tiis/app/fragments/ChildRegisterReportFragment.java
+++ app/src/main/java/mobile/tiis/app/fragments/ChildRegisterReportFragment.java
@@ -1,8 +1,10 @@
 package mobile.tiis.app.fragments;
 
+import android.app.Activity;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,12 +12,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TextView;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -46,6 +52,10 @@ public class ChildRegisterReportFragment extends android.support.v4.app.Fragment
     private RecyclerView childRegisterListView;
     private ChildRegisterReportRecyclerAdapter adapter;
     private ProgressBar progressBar;
+    private MaterialEditText metDOBFrom,metDOBTo;
+    private DatePickerDialog fromDatePicker = new DatePickerDialog();
+    private DatePickerDialog toDatePicker = new DatePickerDialog();
+    private String toDateString = "", fromDateString = "";
 
     public static ChildRegisterReportFragment newInstance(int position) {
         ChildRegisterReportFragment f = new ChildRegisterReportFragment();
@@ -55,11 +65,85 @@ public class ChildRegisterReportFragment extends android.support.v4.app.Fragment
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_child_register, null);
+        final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_child_register, null);
         app = (BackboneApplication) this.getActivity().getApplication();
         this_database = app.getDatabaseInstance();
         this.inflater = inflater;
         setUpView(root);
+
+
+        metDOBFrom = (MaterialEditText) root.findViewById(R.id.met_dob_from);
+        metDOBTo = (MaterialEditText) root.findViewById(R.id.met_dob_value);
+
+
+
+
+        metDOBTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toDatePicker.show(((Activity) getActivity()).getFragmentManager(), "DatePickerDialogue");
+                toDatePicker.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                        metDOBTo.setText((dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth) + "-" + ((monthOfYear + 1) < 10 ? "0" + (monthOfYear + 1) : monthOfYear + 1) + "-"
+                                + year);
+
+                        Calendar toCalendar = Calendar.getInstance();
+                        toCalendar.set(year, monthOfYear, dayOfMonth);
+                        fromDatePicker.setMaxDate(toCalendar);
+                        toDateString = (toCalendar.getTimeInMillis() / 1000) + "";
+
+                        if (!fromDateString.equals("")) {
+                            new filterList().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,fromDateString, toDateString);
+                        } else {
+                            final Snackbar snackbar = Snackbar.make(root, "Please select a start date to view the chart", Snackbar.LENGTH_LONG);
+                            snackbar.setAction("OK", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    snackbar.dismiss();
+                                }
+                            });
+                            snackbar.show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+        metDOBFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fromDatePicker.show(((Activity) getActivity()).getFragmentManager(), "DatePickerDialogue");
+                fromDatePicker.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                        metDOBFrom.setText((dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth) + "-" + ((monthOfYear + 1) < 10 ? "0" + (monthOfYear + 1) : monthOfYear + 1) + "-"
+                                + year);
+
+                        Calendar fromCalendar = Calendar.getInstance();
+                        fromCalendar.set(year, monthOfYear, dayOfMonth);
+                        toDatePicker.setMinDate(fromCalendar);
+                        fromDateString = (fromCalendar.getTimeInMillis() / 1000) + "";
+
+                        if (!toDateString.equals("")) {
+                            new filterList().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,fromDateString, toDateString);
+                        } else {
+                            final Snackbar snackbar = Snackbar.make(root, "Please select an end date to view the report", Snackbar.LENGTH_LONG);
+                            snackbar.setAction("OK", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    snackbar.dismiss();
+                                }
+                            });
+                            snackbar.show();
+                        }
+                    }
+                });
+            }
+        });
+
 
         new filterList().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         return root;
@@ -111,16 +195,19 @@ public class ChildRegisterReportFragment extends android.support.v4.app.Fragment
         @Override
         protected Integer doInBackground(String... params) {
             long t = Calendar.getInstance().getTimeInMillis()/1000;
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.getInstance().get(Calendar.YEAR),1,1);
+
             long t1 = (t + (30 * 24 * 60 * 60));
-            long t2 = (t - (30 * 24 * 60 * 60));
+            long t2 = (c.getTimeInMillis());
             String to_date =  t1+ "";
             String from_date = t2+ "";
 
             try {
-                if (!params[2].equals("") && !params[3].equals("")) {
-                    from_date = (Long.parseLong(params[2])-(24*60*60))+"";
+                if (!params[0].equals("") && !params[1].equals("")) {
+                    from_date = (Long.parseLong(params[0])-(24*60*60))+"";
                     Log.d("day13", "from picker : "+from_date);
-                    to_date = params[3];
+                    to_date = params[1];
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -131,7 +218,7 @@ public class ChildRegisterReportFragment extends android.support.v4.app.Fragment
 
             DatabaseHandler mydb = app.getDatabaseInstance();
 
-            String SQLChildRegistry = "SELECT FIRSTNAME1,FIRSTNAME2,LASTNAME1, BIRTHDATE,GENDER,MOTHER_FIRSTNAME,MOTHER_LASTNAME,MOTHER_VVU_STS,MOTHER_TT2_STS,CUMULATIVE_SERIAL_NUMBER,CHILD_REGISTRY_YEAR, \n" +
+            String SQLChildRegistry = "SELECT DISTINCT child.ID, FIRSTNAME1,FIRSTNAME2,LASTNAME1, BIRTHDATE,GENDER,MOTHER_FIRSTNAME,MOTHER_LASTNAME,MOTHER_VVU_STS,MOTHER_TT2_STS,CUMULATIVE_SERIAL_NUMBER,CHILD_REGISTRY_YEAR, \n" +
                     "\t\t(CASE \n" +
                     "\t\t\tWHEN (place.ID = '-100') \n" +
                     "\t\t\t\tTHEN child.NOTES\n" +
@@ -235,7 +322,10 @@ public class ChildRegisterReportFragment extends android.support.v4.app.Fragment
                     "\t\n" +
                     "         FROM CHILD \n" +
                     "\t\t INNER JOIN \n" +
-                    "\t\t place on child.DOMICILE_ID = place.ID WHERE child.HEALTH_FACILITY_ID = '"+app.getLOGGED_IN_USER_HF_ID()+"'";
+                    "\t\t place on child.DOMICILE_ID = place.ID" +
+                    "     INNER JOIN vaccination_event on child.ID = vaccination_event.CHILD_ID " +
+                    "     WHERE child.HEALTH_FACILITY_ID = '"+app.getLOGGED_IN_USER_HF_ID()+"' " +
+                    "     AND ((substr(vaccination_event.VACCINATION_DATE,7,10)) >= ('" +from_date+ "') AND (substr(vaccination_event.VACCINATION_DATE,7,10)) <= ('" +to_date+ "') AND vaccination_event.VACCINATION_STATUS='true')" ;
 
 
 
@@ -278,7 +368,7 @@ public class ChildRegisterReportFragment extends android.support.v4.app.Fragment
                         row.OPV0 = cursor.getString(cursor.getColumnIndex("OPV0"));
                         row.OPV1 = cursor.getString(cursor.getColumnIndex("OPV1"));
                         row.OPV2 = cursor.getString(cursor.getColumnIndex("OPV2"));
-                        row.OPV0 = cursor.getString(cursor.getColumnIndex("OPV3"));
+                        row.OPV3 = cursor.getString(cursor.getColumnIndex("OPV3"));
                         row.DTP1 = cursor.getString(cursor.getColumnIndex("DTP1"));
                         row.DTP2 = cursor.getString(cursor.getColumnIndex("DTP2"));
                         row.DTP3 = cursor.getString(cursor.getColumnIndex("DTP3"));
