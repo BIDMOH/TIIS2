0a4c12d26d70b22c9f9ab90f0c6e0e17d8aff3df
coze <ilakozejumanne@gmail.com>
2016-10-20 06:46:08 +0300

    Corrected the apps icon and logo image resources

--- appV2/src/main/java/mobile/tiis/appV2/database/DatabaseHandler.java
+++ appV2/src/main/java/mobile/tiis/appV2/database/DatabaseHandler.java
@@ -79,7 +79,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public static final String TAG = "DatabaseHandler";
 
     public static final String DATABASE_NAME = "giis_mobile.db";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
     public static boolean dbPreinstalled = false;
 
     public DatabaseHandler(Context context) {
