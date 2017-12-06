70fe23427f7ae2eb5a51ec6a7666f59609b4c8cf
cozej4 <ilakozejumanne@gmail.com>
2017-12-06 08:01:15 +0300

    Implementation of Postman extraction to Json file that can be synchronized by the server

--- appV2/src/main/java/mobile/tiis/appv2/postman/PostmanSynchronizationService.java
+++ appV2/src/main/java/mobile/tiis/appv2/postman/PostmanSynchronizationService.java
@@ -3,15 +3,24 @@ package mobile.tiis.appv2.postman;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.MySSLSocketFactory;
 import com.loopj.android.http.SyncHttpClient;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -107,257 +116,84 @@ public class PostmanSynchronizationService extends Service {
         aClient.setBasicAuth(userName,password);
 
         Log.d(TAG, ">>>onCreate()");
-        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, NOTIFY_INTERVAL);
+
+        List<PostmanModel> listPosts = db.getAllPosts();
+        Log.d(TAG,"getting all posts");
+        Log.d(TAG,"user = "+userName);
+        Log.d(TAG,"password = "+password);
+        try {
+            sendResult(listPosts.size() + "", getApplicationContext());
+        }catch (Exception e){
+            sendResult("0", getApplicationContext());
+        }
+        JSONArray postmanArray = new JSONArray();
+
+        Log.d(TAG,"postman count  = "+listPosts.size());
+        for (final PostmanModel p : listPosts) {
+//                        sendResult(db.getAllPosts().size()+"",getApplicationContext());
+//                        Log.d(TAG,"url = "+p.getUrl());
+            if(p.getUrl() == null || p.getUrl().trim().equals("")){
+                try {
+                    Thread.sleep(5000);
+                    db.deletePostFromPostman(p.getPostId());
+                    Log.d("POSTMAN DELETED","ID = "+p.getPostId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }else {
+
+                postmanArray.put(p.getUrl());
+            }
+        }
+
+        List<SessionsModel> listModels = app.GetHealthFacilitySessionUpdateUrl();
+
+        for (final SessionsModel p : listModels) {
+            Log.d(TAG,"session url = "+p.getUrl());
+
+            if(p.getUrl() == null || p.getUrl().trim().equals("")){
+                try {
+                    Thread.sleep(5000);
+                    db.updateHealthFacilityStatus(p.get_id(),1);
+                    Log.d("SESSION DELETED","ID = "+p.get_id());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }else{
+                postmanArray.put(p.getUrl());
+            }
+
+        }
+
+        storeToFile(postmanArray);
     }
 
-    class TimeDisplayTimerTask extends TimerTask {
-
-        @Override
-        public void run() {
-
-            Log.d(TAG,"synchronizing postman data");
-            synchronized (app) {
-                List<PostmanModel> listPosts = db.getAllPosts();
-                Log.d(TAG,"getting all posts");
-                Log.d(TAG,"user = "+userName);
-                Log.d(TAG,"password = "+password);
-                try {
-                    sendResult(listPosts.size() + "", getApplicationContext());
-                }catch (Exception e){
-                    sendResult("0", getApplicationContext());
-                }
-                if (listPosts != null && userName != null && password != null) {
-                    Log.d(TAG,"postman count  = "+listPosts.size());
-                    for (final PostmanModel p : listPosts) {
-                        sendResult(db.getAllPosts().size()+"",getApplicationContext());
-                        Log.d(TAG,"url = "+p.getUrl());
-                        if(p.getUrl() == null || p.getUrl().trim().equals("")){
-                            try {
-                                Thread.sleep(5000);
-                                db.deletePostFromPostman(p.getPostId());
-                                Log.d("POSTMAN COMPLETE","url = "+p.getUrl());
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            continue;
-                        }
-
-                        aClient.get(p.getUrl(), new AsyncHttpResponseHandler(true) {
-                            @Override
-                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                                String response = new String(responseBody);
-                                Utils.writeNetworkLogFileOnSD(Utils.returnDeviceIdAndTimestamp(getApplicationContext())+response);
-                                int codeNeg99 = 0;
-                                try{
-                                    JSONObject jobj= null;
-                                    jobj = new JSONObject(response);
-                                    codeNeg99 = jobj.getInt("id");
-
-                                    Log.d(TAG,"received id = "+codeNeg99);
-                                }catch(Exception e){
-                                    e.printStackTrace();
-                                }
-
-                                if(codeNeg99 != -99 && codeNeg99 != -1) {
-                                    boolean res = false;
-                                    do {
-                                        try {
-                                            Thread.sleep(10000);
-                                            res = db.deletePostFromPostman(p.getPostId());
-                                            Log.d("POSTMAN COMPLETE","url = "+p.getUrl());
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
-                                    }while(!res);
-                                }else{
-                                    //check if this was a duplicate barcode registered in the server. if so delete the child
-                                    if(p.getUrl().contains("RegisterChildWithAppoitmentsWithMothersHivStatusAndTT2VaccineStatusAndCatchment")){
-                                        String barcode =  p.getUrl().split("=")[1].substring(0,10);
-                                        Log.d(TAG,"barcode = "+barcode);
-                                        db.removeChildFromChildTable(db.getChildIdByBarcode(barcode));
-
-                                        //In some very minor cases a network connectivity may be disrupted after posting information to the server but before receiving the results
-                                        //this  leads to some scenariors where the data remains in postman and on resending a childregistration request
-                                        // that has already been registered in the server for the second time, the server will keep returning a -1 results code showing that the child is a dublicate.
-                                        //To fix such issues after deletion of a dublicate registration entry from the postman and the database it is important to synch that childs details from the server into the device
-                                        //by using the child's barcode.
-
-                                        //TODO create a custome method for broadcasting child updates to only this specific device and not all other devices with this  child's data
-                                        app.broadcastChildUpdatesWithBarcodeId(barcode);
-
-                                        boolean res1 = false;
-                                        do {
-                                            try {
-                                                Thread.sleep(10000);
-                                                res1 = db.deletePostFromPostman(p.getPostId());
-                                                Log.d("POSTMAN COMPLETE","url = "+p.getUrl());
-                                            } catch (Exception e) {
-                                                e.printStackTrace();
-                                            }
-                                        }while(!res1);
-
-                                    }else {
-                                        if(p.getResponseType()==-5){
-                                            //if the postman fails 10 times delete it from postman
-                                            Log.d(TAG,"response type = "+(p.getResponseType()-1));
-                                            boolean res1 = false;
-                                            do {
-                                                try {
-                                                    Thread.sleep(10000);
-                                                    res1 = db.deletePostFromPostman(p.getPostId());
-                                                } catch (Exception e) {
-                                                    e.printStackTrace();
-                                                }
-                                            }while(!res1);
-
-                                        }else{
-                                            //if the postman fails 10 times delete it from postman
-                                            long res1 = -99;
-                                            do {
-                                                try {
-                                                    Thread.sleep(10000);
-                                                    int responseTypeId = p.getResponseType()-1;
-                                                    Log.d(TAG,"updating response type = "+responseTypeId);
-                                                    Log.d(TAG,"updating url = "+(p.getUrl()));
-                                                    Log.d(TAG,"updating postman id = "+(p.getPostId()));
-                                                    res1 = db.updatePost(p.getUrl(),responseTypeId,p.getPostId());
-                                                    Log.d(TAG,"update post result = "+res1);
-                                                } catch (Exception e) {
-                                                    e.printStackTrace();
-                                                }
-                                            }while(res1==-99);
-
-                                            if(p.getResponseType()-1 <-10){
-                                                db.deletePostFromPostman(p.getPostId());
-                                            }
-
-                                        }
-                                    }
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                                Utils.writeNetworkLogFileOnSD(
-                                        Utils.returnDeviceIdAndTimestamp(getApplicationContext())
-                                                + " StatusCode " + statusCode);
-                                if(p.getResponseType()==-15){
-                                    //if the postman fails 15 times delete it from postman
-                                    Log.d(TAG,"response type = "+(p.getResponseType()-1));
-                                    boolean res1 = false;
-                                    do {
-                                        try {
-                                            Thread.sleep(10000);
-                                            res1 = db.deletePostFromPostman(p.getPostId());
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
-                                    }while(!res1);
-
-                                }else{
-                                    //if the postman fails 10 times delete it from postman
-                                    long res1 = -99;
-                                    do {
-                                        try {
-                                            Thread.sleep(10000);
-                                            int responseTypeId = p.getResponseType()-1;
-                                            Log.d(TAG,"updating response type = "+responseTypeId);
-                                            Log.d(TAG,"updating url = "+(p.getUrl()));
-                                            Log.d(TAG,"updating postman id = "+(p.getPostId()));
-                                            res1 = db.updatePost(p.getUrl(),responseTypeId,p.getPostId());
-                                            Log.d(TAG,"update post result = "+res1);
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
-                                    }while(res1==-99);
-                                }
-
-                                return;
-                            }
-                        });
-                    }
-                }
-
-
-                List<SessionsModel> listModels = app.GetHealthFacilitySessionUpdateUrl();
-
-                if (listModels != null && userName != null && password != null) {
-                    Log.d(TAG,"sessions count  = "+listModels.size());
-                    for (final SessionsModel p : listModels) {
-                        Log.d(TAG,"session url = "+p.getUrl());
-
-
-                        if(p.getUrl() == null || p.getUrl().trim().equals("")){
-                            try {
-                                Thread.sleep(5000);
-                                db.updateHealthFacilityStatus(p.get_id(),1);
-                                Log.d("SESSION COMPLETE","url = "+p.getUrl());
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            continue;
-                        }
-
-                        aClient.get(p.getUrl(), new AsyncHttpResponseHandler(true) {
-                            @Override
-                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                                String response = new String(responseBody);
-                                Utils.writeNetworkLogFileOnSD(Utils.returnDeviceIdAndTimestamp(getApplicationContext())+response);
-                                int codeNeg99 = 0;
-                                try{
-                                    JSONObject jobj= null;
-                                    jobj = new JSONObject(response);
-                                    codeNeg99 = jobj.getInt("id");
-
-                                    Log.d(TAG,"received session id = "+codeNeg99);
-                                }catch(Exception e){
-                                    e.printStackTrace();
-                                }
-
-                                if(codeNeg99 != -1) {
-                                    long res = -1;
-                                    do {
-                                        try {
-                                            Thread.sleep(10000);
-                                            res = db.deleteHealthFacilityStatus(p.get_id());
-                                            Log.d("POSTMAN COMPLETE","url = "+p.getUrl());
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
-                                    }while(res==-1);
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                                Utils.writeNetworkLogFileOnSD(
-                                        Utils.returnDeviceIdAndTimestamp(getApplicationContext())
-                                                + " StatusCode " + statusCode);
-                                return;
-                            }
-                        });
-                    }
-                }
-
-
-                app.parseGCMChildrenInQueueById();
-                app.parseItemLots();
-                app.parseStock();
-            }
-
+    public void sendResult(String message,Context context) {
+        Log.d(TAG,"sending postman count "+message);
+        try {
+            Intent intent = new Intent(CommonUtilities.DISPLAY_POSTMAN_COUNT_ACTION);
+            if (message != null)
+                intent.putExtra(SynchronisationService_MESSAGE, message);
+            context.sendBroadcast(intent);
+        }catch (Exception e){
+            e.printStackTrace();
         }
+    }
 
-        public void sendResult(String message,Context context) {
-            Log.d(TAG,"sending ppostman count "+message);
-            try {
-                Intent intent = new Intent(CommonUtilities.DISPLAY_POSTMAN_COUNT_ACTION);
-                if (message != null)
-                    intent.putExtra(SynchronisationService_MESSAGE, message);
-                context.sendBroadcast(intent);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+
+    public void storeToFile(JSONArray array){
+        Log.d(TAG,"storing data to file  = "+array.toString());
+
+        try {
+            Writer output = null;
+            File file = new File(Environment.getExternalStorageDirectory() + "/tiis/" + "postman.json");
+            output = new BufferedWriter(new FileWriter(file));
+            output.write(array.toString());
+            output.close();
+            Toast.makeText(getApplicationContext(), "postman saved", Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 }
